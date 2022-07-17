e4e803be65c6d3236b8525fdbdda8fa8df544c0f
LakmalSulochana <sulochanalakmal789@gmail.com>
2022-07-17 20:56:03 +0530

    passwrd add part done

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -8,6 +8,30 @@ import GDSEButton from "../../components/common/Button";
 class Login extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            userName: 'admin',
+            pw: 'admin',
+            fillData:{
+                user_name:'',
+                password:''
+            }
+
+        }
+    }
+
+    checkValidity() {
+        console.log("Login button Clicked")
+        console.log("USer name: " + this.state.userName)
+        console.log("password: " + this.state.pw)
+        console.log(this.state.fillData)
+        let fillData=this.state.fillData
+        if (fillData.user_name==this.state.userName && fillData.password==this.state.pw){
+            console.log("login success")
+        }else {
+            console.log("password or user name Didn't match ")
+        }
+
+
     }
 
     render() {
@@ -19,15 +43,35 @@ class Login extends Component {
                         <Typography variant="h4">Login</Typography>
                     </div>
                     <div className={classes.fillContainer}>
-                        <TextField id="outlined-basic" label="User Name" variant="outlined" />
-                        <TextField id="outlined-basic" type="password" label="Password"  variant="outlined" />
+                        <TextField id="outlined-basic"
+                                   label="User Name"
+                                   variant="outlined"
+                                   onChange={(e) => {
+                                       console.log(e.target.value)
+                                       let fillData=this.state.fillData
+                                       fillData.user_name=e.target.value
+                                       this.setState({fillData})
+                                   }}/>
+
+                        <TextField id="outlined-basic"
+                                   type="password" label="Password"
+                                   variant="outlined"
+                                   onChange={(e) => {
+                                       console.log(e.target.value)
+                                       let fillData=this.state.fillData
+                                       fillData.password=e.target.value
+                                       this.setState({fillData})
+                                   }}/>
 
                     </div>
                     <div className={classes.btnContainer}>
-                        <GDSEButton variant="contained" label="Login"/>
+                        <GDSEButton variant="contained" label="Login"
+                                    onClick={() => {
+
+                                        this.checkValidity()
+                                    }}/>
                     </div>
                 </div>
-
 
 
             </div>
