fba841cfd41f32bf9fa83689d94289d7cd46ba8b
LakmalSulochana <sulochanalakmal789@gmail.com>
2022-07-18 20:58:06 +0530

    add axios

--- src/pages/Layout/Grid/index.jsx
+++ src/pages/Layout/Grid/index.jsx
@@ -1,4 +1,4 @@
-import React, {Component , Fragment} from "react";
+import React, {Component, Fragment} from "react";
 import {withStyles} from "@mui/styles";
 import Grid from '@mui/material/Grid';
 import {Autocomplete, TextField, Typography} from "@mui/material";
@@ -8,16 +8,16 @@ import GDSEButton from "../../../components/common/Button";
 class GridLayout extends Component {
     constructor(props) {
         super(props);
-        this.state={
-            top100Films:[{ label: 'City of God', year: 2002 },
-                { label: 'Se7en', year: 1995 },
-                { label: 'The Silence of the Lambs', year: 1991 },
-                { label: "It's a Wonderful Life", year: 1946 },
-                { label: 'Life Is Beautiful', year: 1997 },
-                { label: 'The Usual Suspects', year: 1995 },
-                { label: 'Léon: The Professional', year: 1994 },
-                { label: 'Spirited Away', year: 2001 },
-        ]
+        this.state = {
+            top100Films: [{label: 'City of God', year: 2002},
+                {label: 'Se7en', year: 1995},
+                {label: 'The Silence of the Lambs', year: 1991},
+                {label: "It's a Wonderful Life", year: 1946},
+                {label: 'Life Is Beautiful', year: 1997},
+                {label: 'The Usual Suspects', year: 1995},
+                {label: 'Léon: The Professional', year: 1994},
+                {label: 'Spirited Away', year: 2001},
+            ]
         }
 
     }
@@ -31,35 +31,34 @@ class GridLayout extends Component {
                     Customer Manage
                 </Typography>
                 <Grid container spacing={0.5}>
-                    <Grid item lg={6} md={6} sm={6} xm={6} >
+                    <Grid item lg={6} md={6} sm={6} xm={6}>
                         <Typography variant="body2">
                             Customer Name
                         </Typography>
                         <TextField id="outlined-basic" placeholder="Name" label="Customer Name" variant="outlined"
-                                   size="small" style={{width:'100%'}}/>
+                                   size="small" style={{width: '100%'}}/>
                     </Grid>
-                    <Grid item lg={6} md={6} sm={6} xm={6} style={{width:'100%'}}>
+                    <Grid item lg={6} md={6} sm={6} xm={6} style={{width: '100%'}}>
                         <Autocomplete
                             disablePortal
                             id="combo-box-demo"
                             options={this.state.top100Films}
                             //sx={{ width: 300 }}
-                            renderInput={(params) => <TextField {...params} label="Movie" style={{width:'100%'}} />}
+                            renderInput={(params) => <TextField {...params} label="Movie" style={{width: '100%'}}/>}
                         />
                     </Grid>
-                    <Grid item lg={6} md={6} sm={6} xm={6} >
+                    <Grid item lg={6} md={6} sm={6} xm={6}>
                         <Typography variant="body2">
                             Customer NIC
                         </Typography>
-                        <TextField id="outlined-basic" placeholder="NIC" label="Customer NIC" variant="outlined"
-                                   size="small" style={{width:'100%'}}/>
+                        <TextField id="outlined-basic" placeholder="NIC" label="Customer NIC" variant="outlined" size="small" style={{width: '100%'}}/>
                     </Grid>
                     <Grid item lg={6} md={6} sm={6} xm={6}>
                         <Typography variant="body2">
                             Customer Address
                         </Typography>
                         <TextField id="outlined-basic" placeholder="address" label="Customer Address" variant="outlined"
-                                   size="small" style={{width:'100%'}}/>
+                                   size="small" style={{width: '100%'}}/>
                     </Grid>
                     <Grid item lg={6} md={6} sm={6} xm={6}>
 
